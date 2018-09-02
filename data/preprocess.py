64376867276bc44f58b7b807e5e6694ef3991583
cheese <goho302jo03@gmail.com>
2018-09-02 20:07:25 +0800

    Fix preprocess.py

--- data/preprocess.py
+++ data/preprocess.py
@@ -4,8 +4,10 @@ def main(file):
     with open('%s.dat' %file, 'r') as f:
         data = f.readlines()
 
-    data = [v.split() for v in data]
-    np.save('./%s.npy' %file, np.array(data, dtype='float32'))
+    data = np.array([v.split() for v in data], dtype='float32')
+    if file == 'german':
+        data[:, -1] -= 1
+    np.save('./%s.npy' %file, data)
 
 if __name__ == "__main__":
     main('australian')
