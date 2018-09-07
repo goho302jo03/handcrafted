20a266cd2142e4100f3991f60162bc2072d22000
cheese <goho302jo03@gmail.com>
2018-09-07 15:20:24 +0800

    Modify split.py

--- data/split.py
+++ data/split.py
@@ -1,9 +1,14 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-def split(file, seed):
+def old_split(file, seed):
     data = np.load(file)
     train, tmp, _, _ = train_test_split(data, data, test_size=0.2, random_state=seed)
     val, test, _, _ = train_test_split(tmp, tmp, test_size=0.5, random_state=seed)
 
     return train, val, test
+
+def split(file):
+    data = np.load(file)
+
+    return data
