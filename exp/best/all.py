ae3e6ba7c8def191418e3122431dfe98927b3458
cheese <goho302jo03@gmail.com>
2018-11-28 21:46:42 +0800

    Refine best

--- exp/best/all.py
+++ exp/best/all.py
@@ -138,7 +138,7 @@ if __name__ == '__main__':
         print('------------------------------------------------')
         print('Seed = %d' %(random_seed))
         print('------------------------------------------------')
-        train, tmp, _, _ = train_test_split(datas, datas, test_size=0.2, random_state=random_seed)
+        train, tmp, _, _ = train_test_split(datas, datas, test_size=0.4, random_state=random_seed)
         val, test, _, _ = train_test_split(tmp, tmp, test_size=0.5, random_state=random_seed)
 
         x_train, y_train = train[:, :-1], train[:, -1]
